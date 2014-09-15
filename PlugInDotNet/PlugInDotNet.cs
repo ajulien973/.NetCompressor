b92016edde6bbebcfc0a2cb789f46f25121dcc77
ekanseht <alexis.julien@edu.esiee.fr>
2014-09-15 12:08:45 +0200

    blah

---
+++ PlugInDotNet/PlugInDotNet.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Huffman;
+
+namespace PlugInDotNet
+{
+    public class PlugInDotNet : MarshalByRefObject, IPlugin 
+    {
+        public bool Compress(ref Huffman.HuffmanData data)
+        {
+            bool r = false;
+
+            data.sizeOfUncompressedData = data.uncompressedData.Length;
+            data.frequency = new List<KeyValuePair<byte, int>>();
+
+            Dictionary<byte, int> d = new Dictionary<byte, int>();
+
+            foreach(byte b in data.uncompressedData)
+            {
+                if (d.ContainsKey(b))
+                {
+                    d[b] = d[b] + 1;
+                }
+                else
+                {
+                    d.Add(b, 1);
+                }
+            }
+
+            foreach (byte b in d.Keys)
+            {
+                data.frequency.Add(new KeyValuePair<byte,int>(b, d[b]));
+            }
+
+            return r;
+        }
+
+        public bool Decompress(ref Huffman.HuffmanData data)
+        {
+            return false;
+        }
+
+        public string PluginName {
+            get { return "";  }
+        }
+    }
+}
---
+++ TestPlugInDotNet/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PlugInDotNet;
+
+namespace TestPlugInDotNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Huffman.HuffmanData datas = new Huffman.HuffmanData();
+            datas.uncompressedData = Encoding.ASCII.GetBytes("azerab");
+            PlugInDotNet.PlugInDotNet p = new PlugInDotNet.PlugInDotNet();
+            p.Compress(ref datas);
+
+            System.Console.WriteLine(Encoding.ASCII.GetString(datas.uncompressedData));
+
+            foreach (KeyValuePair<byte, int> kvp in datas.frequency)
+            {
+                System.Console.WriteLine(Encoding.ASCII.Get(kvp.Key) + " - " + Convert.ToString(kvp.Value));
+            }
+        }
+    }
+}
